1b11a9b5a906e904b62de310b1609959f3310a14
Douglas Everton de Oliveira <douglas.oliveirabh@gmail.com>
2018-02-14 09:38:24 -0200

    Adding person api and authentication with identity server

--- src/Services/Identity/Identity.Api/Config.cs
+++ src/Services/Identity/Identity.Api/Config.cs
@@ -6,17 +6,37 @@ namespace Identity.Api
 {
     public class IdentityServerConfig
     {
-        public static IEnumerable<ApiResource> GetApiResources() {
-            return null;
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>(){
+                new ApiResource("PersonApi", "Person Api")
+            };
         }
 
-        public static IEnumerable<IdentityResource> GetIdentityResources() {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
             return new List<IdentityResource>(){
                 new IdentityResources.OpenId(),
                 new IdentityResources.Address(),
                 new IdentityResources.Email(),
                 new IdentityResources.Phone(),
                 new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client() {
+                        ClientId = "PersonApiClient",
+                        ClientName = "Person Api Client",
+                        AllowedGrantTypes = GrantTypes.ClientCredentials,
+                        ClientSecrets = new List<Secret>(){
+                            new Secret("p@ssw0rd".Sha256())
+                        },
+                        AllowedScopes = new List<string>{ "PersonApi.Read"}
+                }
             };
         }
     }
--- src/Services/Person/Person.Api/Startup.cs
+++ src/Services/Person/Person.Api/Startup.cs
@@ -4,32 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
-namespace Identity.Api
+namespace Person.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-
             services
-                .AddIdentityServer()                
-                .AddDeveloperSigningCredential()
-                .AddInMemoryApiResources(IdentityServerConfig.GetApiResources())
-                .AddInMemoryClients(IdentityServerConfig.GetClients());
+                .AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+
+
+            services.AddAuthentication("Bearer")
+            .AddIdentityServerAuthentication(options =>
+            {
+                options.Authority = "http://localhost:5000"; //mudar configuração para o arquivo
+                options.RequireHttpsMetadata = false;
+
+                options.ApiName = "PersonApi";
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,9 +40,9 @@ namespace Identity.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseAuthentication();
 
-            app.UseIdentityServer();
+            app.UseMvc();
         }
     }
 }
